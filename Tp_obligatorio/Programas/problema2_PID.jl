f6d4bee2841690f5b341d2d9092c9c33d0009451
elsuizo <mnoblia@disroot.org>
2017-12-08 20:21:56 -0300

    update PID

--- Tp_obligatorio/Programas/problema2_PID.jl
+++ Tp_obligatorio/Programas/problema2_PID.jl
@@ -32,24 +32,25 @@ const  R      =  2.390057361376673e-5
 const  cₑ     =  4.184e6
 const  V      =  0.5
 const  θ_ref  =  333.0
-const  θ₀     =  288.0
+θ₀     =  [288.0]
 const  θₐ    =  293.0
 G_inv = 0.4 * 836800.0
 
-
+f(u) = u[2]
 function heat_panel(t, θ, dθ)
-   dθ[1] = u[1] /(cₑ * V) + (A * G_inv)/(cₑ * V * R) - (θ[1] - θₐ) / (cₑ * V * R)
+   dθ[1] = (f(u) / (cₑ * V)) + ((A * G_inv))/((cₑ * V * R)) - ((θ[1] - θₐ)) / ((cₑ * V * R))
 end
 #=------------------------------------------------------------------------------
                         constants of the algorithm
 ------------------------------------------------------------------------------=#
-K_c = 1
-tauI = 0.913444964569
+K_p = 41840
+K_i = 836.8
+tauI = 110
 tauD = 0.0
-t_sim = 100
-δt  = .1
-time_sim = linspace(0, t_sim)
-set_point = zeros(time_sim) # zeros like t
+t_sim = 1000
+δt  = .01
+time_sim = collect(0:δt:t_sim)
+set_point = ones(time_sim) * θ_ref
 error = zeros(time_sim) # zeros like t
 θ = zeros(time_sim) # zeros like t
 dθ = zeros(time_sim) # zeros like t
@@ -60,36 +61,36 @@ D = zeros(time_sim)
 pv = zeros(time_sim)
 controller_out = zeros(time_sim)
 # Upper and Lower limits on OP
-controller_max = 350.0
-controller_min = 250.0
+controller_max = 600.0
+controller_min = -100.0
 
+pv[1] = θ_ref
 u = ones(time_sim) * θ_ref
-for sample in 0:t_sim
+for sample in 0:length(time_sim) - 2
    Δt = time_sim[sample+1] - time_sim[sample+2]
    error[sample+1] = set_point[sample+1] - pv[sample+1]
-   P[sample+1] = K_c * error[sample+1]
-   I[sample+1] = K_c/tauI * error_int[sample+1]
-   D[sample+1] = - K_c * tauD * dθ[sample+1]
+   if sample >= 1  # calculate starting on second cycle
+     error_int[sample+1] = error_int[sample] + error[sample+1] * Δt
+   end
+   P[sample+1] = K_p * error[sample+1]
+   I[sample+1] = K_i* error_int[sample+1]
    controller_out[sample+1] = controller_out[1] + P[sample+1] + I[sample+1] + D[sample+1]
-   if controller_out[sample+1] > controller_max  # check upper limit
-      controller_out[sample+1] = controller_max
-      error_int[sample+1] = error_int[sample+1] - error[sample+1] * Δt # anti-reset windup
-   end
-   if controller_out[sample+1] < controller_min  # check lower limit
-      controller_out[sample+1] = controller_min
-      error_int[sample+1] = error_int[sample+1] - error[sample+1] * Δt # anti-reset windup
-   end
+   #= if controller_out[sample+1] > controller_max  # check upper limit =#
+   #=    controller_out[sample+1] = controller_max =#
+   #=    error_int[sample+1] = error_int[sample+1] - error[sample+1] * Δt # anti-reset windup =#
+   #= end =#
+   #= if controller_out[sample+1] < controller_min  # check lower limit =#
+   #=    controller_out[sample+1] = controller_min =#
+   #=    error_int[sample+1] = error_int[sample+1] - error[sample+1] * Δt # anti-reset windup =#
+   #= end =#
    ts = [time_sim[sample+1],time_sim[sample+2]]
+   #= @show ts =#
    u[sample+2]= controller_out[sample+1]
    sol = Sundials.cvode(heat_panel, θ₀,ts)
+   @show sol
    θ[sample+2] = sol[end]
-   θ₀ = θ[sample+2]
+   θ₀[1] = θ[sample+2]
    pv[sample+2] = θ[sample+2]
 end
-#= controller_out[len(t)-1] = controller_out[len(t)-2] =#
-#= ie[len(t)-1] = ie[len(t)-2] =#
-#= P[len(t)-1] = P[len(t)-2] =#
-#= I[len(t)-1] = I[len(t)-2] =#
-#= D[len(t)-1] = D[len(t)-2] =#
-#=  =#
 
+#= plot() =#
