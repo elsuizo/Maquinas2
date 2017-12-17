bb347eff90ae78b2c33177173c25346f235c6591
elsuizo <mnoblia@disroot.org>
2017-12-17 18:22:32 -0300

    update with PID simulation

--- Tp_obligatorio/Programas/problema2_PID.jl
+++ Tp_obligatorio/Programas/problema2_PID.jl
@@ -22,7 +22,7 @@
 #
 # You should have received a copy of the GNU General Public License
 ---------------------------------------------------------------------------=#
-#= using Sundials =#
+using Sundials
 using PyPlot
 #=------------------------------------------------------------------------------
                               constants
@@ -31,37 +31,41 @@ const  A      =  2.0
 const  R      =  2.390057361376673e-5
 const  cₑ     =  4.184e6
 const  V      =  0.5
-const  θ_ref  =  333.0
-θ₀     =  [288.0]
-const  θₐ    =  293.0
-G_inv = 0.4 * 836800.0
-q_nec = ((θ_ref - θ_a) / (R)) - A * G_inv
-τ = R * cₑ * V
-function heat_panel(t, θ, dθ)
-   dθ[1] = ((R/τ) * u * exp((-t[1])/(τ))/ (cₑ * V)) + ((A * G_inv))/((cₑ * V * R)) - ((θ[1] - θₐ)) / ((cₑ * V * R))
-const  θₐ     =  293.0
-θ₀     =  281.0
-const  τ      = R * cₑ * V
+const  θ_ref  =  60.0 + 273.0
+const θ₀_inicial     =  rand(273.0:290.0)
 const  G_inv  = 0.4 * 836800.0
-const  K_p    = 41840.0
-q_sup = (θ_ref - θₐ)/(R) - A * G_inv
-τ = R * cₑ * V
-function heat_panel_winter(t, A, θ₀, θₐ, τ, G, u)
-   (u * R * exp(-t/τ)) / (τ) + ((θₐ + R*A*G) * (1 - exp(-t / τ))) + θ₀ * exp(-t / τ)
+const  θₐ    =  293.0
+q_nec = ((θ_ref - θₐ) / (R)) - A * G_inv
+const  τ      = R * cₑ * V
+α = cₑ * V
+f(u) = u[2]
+# funcion para generar una perturbacion aleatoria escalon
+θ_a_f(t) = (100.0 < t < 300.0) ? θₐ - rand(1.0:10.0): θₐ
+
+"""
+Funcion que modela el problema 2
+"""
+function heat_panel(t, θ, dθ)
+   dθ[1] = (f(u) / (α)) + ( ((A * G_inv)) / α - ((θ[1] - θₐ) / τ ) + q_nec / α)
+end
+
+function heat_panel_pert(t, θ, dθ)
+   dθ[1] = (f(u) / (α)) + ( ((A * G_inv)) / α - ((θ[1] - θ_a_f(t)) / τ ) + q_nec / α)
 end
 #=------------------------------------------------------------------------------
                         constants of the algorithm
 ------------------------------------------------------------------------------=#
-Kp = 1 / R
-Ki = 837.0
+K_p = 1 / R
+K_i = 837.0
 tauI = 110
 tauD = 0.0
 t_sim = 500.0
 δt  = .01
-time_sim = linspace(0, 500, 10000-1)
+time_sim = linspace(0, 500, 10000)
 set_point = zeros(time_sim) * θ_ref
 error = zeros(time_sim) # zeros like t
 θ = zeros(time_sim) # zeros like t
+#= θ₀ = zeros(time_sim) # zeros like t =#
 dθ = zeros(time_sim) # zeros like t
 error_int = zeros(time_sim) # zeros like t
 P = zeros(time_sim)
@@ -73,11 +77,9 @@ controller_out = zeros(time_sim)
 controller_max = 3e6
 controller_min = 0.0
 
+θ₀ = zeros(1)
+θ₀[1] = θ₀_inicial
 controller_min = 0
-#= v1 = view(set_point, 1:100) =#
-#= v2 = view(set_point, 100:300) =#
-#= #= fill!(v1, 200) =# =#
-#= fill!(v2, 333.0) =#
 pv[1] = θ_ref
 u = ones(time_sim) * θ_ref
 for sample in 0:length(time_sim) - 2
@@ -88,12 +90,6 @@ for sample in 0:length(time_sim) - 2
    end
    P[sample+1] = K_p * error[sample+1]
    I[sample+1] = K_i* error_int[sample+1]
-   controller_out[sample+1] = controller_out[1] + P[sample+1] + I[sample+1] + D[sample+1]
-   P[sample+1] = Kp * error[sample+1]
-   I[sample+1] = Ki* error_int[sample+1]
-   controller_out[sample+1] = controller_out[1] + P[sample+1] + I[sample+1]
-   P[sample+1] = Kp * error[sample+1]
-   I[sample+1] = Ki* error_int[sample+1]
    controller_out[sample+1] = controller_out[1] + P[sample+1] + I[sample+1]
    if controller_out[sample+1] > controller_max  # check upper limit
       controller_out[sample+1] = controller_max
@@ -106,9 +102,17 @@ for sample in 0:length(time_sim) - 2
    ts = [time_sim[sample+1],time_sim[sample+2]]
    #= @show ts =#
    u[sample+2]= controller_out[sample+1]
-   sol = heat_panel_winter(time_sim[sample+1], A, θ₀, θₐ, τ, G_inv,u[sample+1])
+   sol = Sundials.cvode(heat_panel_pert, θ₀,ts)
+   #= sol = integrate.odeint(heat_panel, θ₀[1], ts) =#
    θ[sample+2] = sol[end]
-   θ₀ = θ[sample+2]
+   θ₀[1] = θ[sample+2]
    pv[sample+2] = θ[sample+2]
 end
 
+plot(time_sim[2:end], θ[2:end] - 273.0)
+xlabel(L"t\,[seg]")
+ylabel(L"T\,[^{\circ}C]")
+grid("on")
+matplotlib["rcParams"][:update](["font.size" => 14, "font.family" => "serif"])
+text(0, θ₀_inicial - 273.0, latexstring("\\theta_{0}="*"$(θ₀_inicial-273.0)"), fontsize=10)
+legend()
